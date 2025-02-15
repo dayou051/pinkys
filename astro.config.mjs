d9494bc8a99f9ce6a02956b721dda918f9174ebb
Eduardo Alayo Vargas <eduardoalayovargas@gmail.com>
2025-02-15 12:27:25 -0500

    Modificaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import path from 'path';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
